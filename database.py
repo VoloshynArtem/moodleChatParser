32c96183c500c2aa2f89fc766e5f4afb4ed1df1f
VoloshynArtem <vartem.gg@gmail.com>
2025-10-23 11:56:32 +0200

    fix unable to create db

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 import os
 from dotenv import load_dotenv
+from contextlib import contextmanager
 
 
 load_dotenv()
@@ -17,13 +18,28 @@ def init_database():
     _create_database_if_not_exists()
     _create_table_if_not_exists()
 
+@contextmanager
+def _no_transaction_connection(dbname="postgres", user = os.getenv("NEW_USER"), password=os.getenv("NEW_USER_PASSWORD")):
+    conn = psycopg2.connect(
+            dbname=dbname,
+            user=user, 
+            password=password, 
+            host="localhost"
+            )
+    try:
+        conn.autocommit = True
+        yield conn
+    finally:
+        conn.close()
+
 def _connect_to_db(dbname = new_db_name, user = os.getenv("NEW_USER"), password=os.getenv("NEW_USER_PASSWORD")):
     return psycopg2.connect(
             dbname=dbname,
             user=user,
             password=password,
-            host="localhost"
+            host="localhost",
         )
+
 
 def _create_user_if_not_exists():
     new_user = os.getenv("NEW_USER")
@@ -45,20 +61,20 @@ def _create_user_if_not_exists():
 
 
 def _create_database_if_not_exists():
-    with _connect_to_db("postgres", os.getenv("ADMIN_USER"), os.getenv("ADMIN_PASSWORD")) as conn:
-            with conn.cursor() as cur:
-                # Check if database exists
-                cur.execute("SELECT 1 FROM pg_database WHERE datname=%s", (new_db_name,))
-                exists = cur.fetchone()
+    global new_db_name
+    new_user = os.getenv("NEW_USER")
+    with _no_transaction_connection("postgres", os.getenv("ADMIN_USER"), os.getenv("ADMIN_PASSWORD")) as conn:
+        with conn.cursor() as cur:
+            # Check if database exists
+            cur.execute("SELECT 1 FROM pg_database WHERE datname=%s", (new_db_name,))
+            exists = cur.fetchone()
 
-                if not exists:
-                    cur.execute(sql.SQL("CREATE DATABASE {} OWNER {}").format(
-                        sql.Identifier(new_db_name),
-                        sql.Identifier(new_user)
-                    ))
-                    print(f"Database '{new_db_name}' created with owner '{new_user}'.")
-                
-
+            if not exists:
+                cur.execute(sql.SQL("CREATE DATABASE {} OWNER {}").format(
+                    sql.Identifier(new_db_name),
+                    sql.Identifier(new_user)
+                ))
+                print(f"Database '{new_db_name}' created with owner '{new_user}'.")
 
 
 def _create_table_if_not_exists():
@@ -71,7 +87,7 @@ def _create_table_if_not_exists():
                     msg_id TEXT NOT NULL UNIQUE,
                     username TEXT NOT NULL,
                     message TEXT NOT NULL,
-                    time TIME NOT NULL
+                    timestamp TIMESTAMP NOT NULL
                 );
             """).format(sql.Identifier(table_name)))
             conn.commit()
@@ -81,17 +97,18 @@ def _create_table_if_not_exists():
 
 def insert_chat(msg_id, username, message, time_str):
     try:
-        time_obj = datetime.strptime(time_str, "%H:%M").time()
+        today = datetime.today().date()
+        timestamp = datetime.strptime(f"{today} {time_str}", "%Y-%m-%d %H:%M")
 
         with _connect_to_db() as conn:
             with conn.cursor() as cur:   
 
                 insert_query = sql.SQL("""
-                    INSERT INTO {} (msg_id, username, message, time)
+                    INSERT INTO {} (msg_id, username, message, timestamp)
                     VALUES (%s, %s, %s, %s);
                 """).format(sql.Identifier(table_name))
 
-                cur.execute(insert_query, (msg_id, username, message, time_obj))
+                cur.execute(insert_query, (msg_id, username, message, timestamp))
                 conn.commit()
                 
     except psycopg2.Error as e:
@@ -113,6 +130,4 @@ def check_exists(msg_id):
                 """
             )
             return cur.fetchone()[0]
-
-
 
